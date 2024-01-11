a3266e00c3ce021462693925234df139de875153
Aldi Doanta Kurnia <aldidkurnia@gmail.com>
2024-01-11 17:30:15 +0700

    up to '39.Project Review'

---
+++ cards/deck_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 12 {
+		t.Errorf("Expected deck length of 12, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Three of Diamonds" {
+		t.Errorf("Expected last card of Three of Diamonds, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 12 {
+		t.Errorf("Expected deck length of 12, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
